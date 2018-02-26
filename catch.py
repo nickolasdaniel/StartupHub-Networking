a86bc60c69cd4160261859eed17aed80684d7363
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2018-02-26 10:58:37 -0800

    Update catch.py

--- catch.py
+++ catch.py
@@ -20,15 +20,6 @@ class WebCacheProxy(object):
     def __contains__(self, key):
         return key in self.dict
 
-#     def __iter__(self):
-#         self.current_entry = -1
-#         return self
-
-#     def __next__(self):
-#         if self.current_entry >= self.number_of_elements() - 1:
-#             raise StopIteration()
-#         return list(self.dict.values())[self.current_entry]
-
     def add_entry_cache(self, key, value):
         if len(self.dict) >= self.limit and key not in self.dict:
             self.remove_entry_cache()
@@ -45,11 +36,11 @@ class WebCacheProxy(object):
 
         del self.dict[auxkey]
 
-#     def get_entry_cache(self, key):
-#         http_pattern = re.compile("htt[ps]://[a-zA-Z0-9].[a-z]{3} +")
-#         if key not in self.dict and http_pattern.search(key) is None:
-#             raise KeyError('Request not in cache or doesn`t match the pattern')
-#         return self.dict[key]
+    # def get_entry_cache(self, key):
+    #     http_pattern = re.compile("htt[ps]://[a-zA-Z0-9].[a-z]{3} +")
+    #     if key not in self.dict and http_pattern.search(key) is None:
+    #         raise KeyError('Request not in cache or doesn`t match the pattern')
+    #     return self.dict[key]
 
     def number_of_elements(self):
         return len(self.dict)
@@ -58,8 +49,5 @@ if __name__ == '__main__':
     e = Entry('http://index.html', datetime.datetime.now())
     c = WebCacheProxy()
     c.add_entry_cache('http://www.facebook.com', e)
-    c.add_entry_cache('http://www.google.com', e)
-    c.add_entry_cache('http://www.youtube.com', e)
 
-    for e in c:
-        print(e)
+    print(e)
