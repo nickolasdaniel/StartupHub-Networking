21a0e71ebdc5ee81b7a6a579618b4d669f55e712
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2018-03-09 19:55:16 +0000

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -1,69 +1,80 @@
 import socket
+import time
 
-class Proxy_Server(object):
-    def __init__(self,server_address,server_address2):
-        self.server_address=server_address
-        self.server_address2=server_address2
+class ProxyServer(object):
+    def __init__(self, server_address, server_address2):
+        self.server_address = server_address
+        self.server_address2 = server_address2
 
-        self.socket_server1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        try:
-            self.socket_server1.bind(server_address)
-        except socket.error as err:
-            print(str(err))
-            
-        self.setsock(self.socket_server1)
-        
+        self.socket_server1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        print("[*] Binding server...")
+        self.socket_server1.bind(self.server_address)
+        print("[*] Server binded successfully.")
+
+        print("[*] Server is now listening...")
         self.socket_server1.listen(5)
-        
-        
-        print("listen at {}:{}".format(server_address[0],server_address[1]))
 
-        self.client_server,self.clent_addr=self.socket_server1.accept()
+        self.client_socket, self.client_addr = self.socket_server1.accept()
 
-
-        self.socket_server2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket_server2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_server2.connect(server_address2)
-
-        print("connected at {}:{}".format(server_address2[0],server_address2[1]))
+        print("[*] Client has just connected.")
 
         while True:
-            try:
-                self.handle(self.socket_server2,self.client_server)
-            except KeyboardInterrupt:
-                print("erroare fam")
-                
-    def setsock(self,socket_server1):
-        self.socket_server1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        if hasattr(socket, "SO_REUSEPORT"):
-            self.socket_server1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            self.handle(self.socket_server2, self.client_socket)
 
-    def handle(self,socket_server2,client_server):
-        self.MAX_RECV_BUFFER=1024
-        self.data=client_server.recv(self.MAX_RECV_BUFFER)
-
-
-        if not self.data:
-            print("client just disconected")
-            self.client_server.close()
-            self.socket_server2.close()
-
-        socket_server2.send(self.data)
-        print("data send {}".format(self.data))
+    def handle(self, socket_server2, client_socket):
+        self.MAX_RECV_BUFF = 1024
+        self.socket_server2 = socket_server2
+        self.client_socket = client_socket
 
         try:
-            self.data2=socket_server2.recv(self.MAX_RECV_BUFFER)
+            self.data = self.client_socket.recvall(self.client_socket)
+            print("[*] Received data from client: {}".format(self.data))
+
+            if not self.data:
+                print("[*] Client just disconnected.")
+                self.socket_server2.close()
+                self.client_socket.close()
+
+            print("[*] Sending data to the server...")
+            self.socket_server2.send(self.data)
+            print("[*] Data has been sent.")
+        except socket.error as serr:
+            print(serr)
+
+        try:
+            self.data2 = self.socket_server2.recv(self.MAX_RECV_BUFF)
+            print("[*] Received data back from the server.")
 
             if not self.data2:
+                print("[*] Server stopped working")
+                self.client_socket.close()
+                self.socket_server2.close()
 
-                print("server stopped working")
-                client_server.close()
-                socket_server2.close()
-
-            client_server.send(self.data2)
-            print("data to {}".format(self.data2))
-
+            print("[*] Sending data to client...")
+            self.client_socket.send(self.data2)
+            print("[*] Data has been sent.")
         except socket.error as serr:
-            print(str(serr))
+            print(serr)
 
-if __name__=="__main__":
-    Proxy_Server(("127.0.0.1",8081),("127.0.0.1",8089))
+
+    # def recvall(self, client_socket, MAX_RECV_BUFF=1024):
+    #     self.client_socket = client_socket
+    #
+    #     self.MAX_RECV_BUFF = MAX_RECV_BUFF
+    #
+    #     self.total_data = []
+    #     self.data = ""
+    #
+    #     while 1:
+    #         self.data = client_socket.recv(self.MAX_RECV_BUFF)
+    #         self.total_data.append(self.data)
+    #         if len(self.data) < self.MAX_RECV_BUFF:
+    #             break
+    #     return self.total_data
+
+
+if __name__ == "__main__":
+    ProxyServer(("127.0.0.1", 8081),("127.0.0.1",8089))
