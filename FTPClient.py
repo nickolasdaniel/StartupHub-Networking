d7caeae45965da8abc37a856166b7a159e451278
DragosCamerisru <36566111+DragosCamerisru@users.noreply.github.com>
2018-03-17 13:36:07 +0200

    Update FTPClient.py

--- FTPClient.py
+++ FTPClient.py
@@ -1,18 +1,17 @@
 import socket
 import os
-import struct
-import sys 
 
 class FTPClient(object):
 
-    DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    FILENAME = 'mata3'
-    MAX_RECV_SIZE = 1024
-
-    def __init__(self, server_address):
+    BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    FILENAME='mata1027'
+    def __init__(self, server_address,reuseAddr=True):
         self.server_address = server_address
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        if reuseAddr:
+            self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
     def start_client(self):
         try:
             self.client_socket.connect(self.server_address)
@@ -22,28 +21,20 @@ class FTPClient(object):
             print("[*] Couldn`t connect.")
 
     def download_file(self, filename):
-        self.unpacker = struct.Struct("I")
-        self.file_size = unpacker.unpack(self.client_socket.recv(self.unpacker.size))[0]
-        if os.path.exists(FTPClient.DOWNLOAD_FOLDER):
-            self.file_location = FTPClient.DOWNLOAD_FOLDER + filename
-            with open(filename,'wb') as self.f:
-                print("[*] File has been created...")
-                self.recv_len = 0
-                try:
-                    while self.recv_len <= self.file_size:
-                        self.data = self.client_server.recv(FTPClient.MAX_RECV_SIZE)
-                        self.f.write(self.data)
-                        self.recv_len += len(self.file_data)
-                except IOError as ierr:
-                    sys.exit(1)
-                 print("File: {} Bytes: {} was succesfuly downloaded".format(filename, self.file_size))
-         else:
-            self.client_socket.send("Download location doesn`t exist!")
-                    
-            
-        self.f.close()
+        with open(filename,'w') as f:
+            print("file opened...")
+            self.rec_data=''
+            self.size=1
+            while self.size:
+                self.size=""
+                print("reciving data...")
+                self.data = self.client_socket.recv(1024)
+                self.data = self.data.decode('utf-8')
+                self.size=self.data
+                self.rec_data+=self.data
+            f.write(self.rec_data)
         self.client_socket.close()
 
 if __name__ == "__main__":
-    ftp = FTPClient(("127.0.0.1", 8080))
+    ftp = FTPClient(("127.0.0.1", 8089))
     ftp.start_client()
