619bce795dcd33332b3137f3dcd5b643858e54a8
DragosCamerisru <36566111+DragosCamerisru@users.noreply.github.com>
2018-03-23 08:41:27 +0200

    Update muie.py

--- muie.py
+++ muie.py
@@ -1,10 +1,14 @@
-import ConfigParser
-
-config=ConfigParser.ConfigParser()
+try:
+    import ConfigParser
+    config=ConfigParser.ConfigParser()
+except:
+    from configparser import ConfigParser
+    config=ConfigParser()
+  
 config.read('confiv_server.ini')
 sections=config.sections()
 for section in sections:
     options=config.options(section)
     for option in options:
         value=config.get(section,option)
-        print option,value
+        print(option,value)
